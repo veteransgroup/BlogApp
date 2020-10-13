f61cbdc1227ab65681fcddf4ea65449dabfc7e8c
Jeff Liu <jeff.liu@outlook.co.nz>
2020-10-14 09:21:42 +1300

    add elasticsearch

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -1,8 +1,8 @@
 from .common import *
 
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = '8k@0h5kklahg9!650a6jdsx80on6no$hwtsnawqxt0&4macd7h'                 # os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-HAYSTACK_CONNECTIONS['default']['URL'] = 'http://django_blog_elasticsearch:9200/'
+HAYSTACK_CONNECTIONS['default']['URL'] = 'http://elasticsearch:9200/'
